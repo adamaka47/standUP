18da3b73ea9059e66b98091c91f5fb147c2ba9f6
Adam2 <43184258+adamaka47@users.noreply.github.com>
2019-03-04 16:49:03 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
+$to = "lol11211223@yandex.ru";
+$subject = "Заявка от стендапа";
+// Сообщение письма
+$message = "
+Имя пользователя: ".htmlspecialchars($name)."<br />
+Телефон: <a href='tel:$phone'>".htmlspecialchars($phone)."</a>";
+// Отправляем письмо при помощи функции mail();
+$headers = "From: unknown.ru <unknown@hz.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+mail ($to, $subject, $message, $headers);
+// Перенаправляем человека на страницу благодарности и завершаем скрипт
+header('Location: thanks.html');
+exit();
+?>
